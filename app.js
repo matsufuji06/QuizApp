66fc5fb3ad79ae4011e169664ca405afceda166c
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-07 20:30:55 +0900

    選択肢の表示

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const answers = ["1", "0", "2", "3"];
 const correct = "2";
 
 document.getElementById("js-question").textContent = question;
+
+document.getElementsByTagName("button")[0].textContent = answers[0];
+document.getElementsByTagName("button")[1].textContent = answers[1];
+document.getElementsByTagName("button")[2].textContent = answers[2];
+document.getElementsByTagName("button")[3].textContent = answers[3];
