668c27f7062528e5e187a460d03b9713b9472364
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-08 13:20:06 +0900

    if分岐で正解か不正解のアラートを表示

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const question = "1 + 1 = ";
-const answers = ["1", "0", "2", "3"];
-const correct = "2";
+const question = "マイケル・スコフィールドの名言は？ ";
+const answers = ["お肉食べたい", "信じることだ", "脱獄無理ゲーwww", "展開だらだらしすぎ"];
+const correct = "信じることだ";
 
 document.getElementById("js-question").textContent = question;
 
@@ -8,3 +8,11 @@ document.getElementsByTagName("button")[0].textContent = answers[0];
 document.getElementsByTagName("button")[1].textContent = answers[1];
 document.getElementsByTagName("button")[2].textContent = answers[2];
 document.getElementsByTagName("button")[3].textContent = answers[3];
+
+document.getElementsByTagName("button")[0].addEventListener("click", ()=>{
+  if( correct === document.getElementsByTagName("button")[0].textContent){
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
