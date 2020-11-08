204fadfa30925a5a923ffd17b2c0b535826d8f74
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-08 13:27:07 +0900

    document.getElementsByTagName("button");を変数化

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const question = "マイケル・スコフィールドの名言は？ ";
-const answers = ["お肉食べたい", "信じることだ", "脱獄無理ゲーwww", "展開だらだらしすぎ"];
+const answers = [
+  "お肉食べたい", 
+  "信じることだ", 
+  "脱獄無理ゲーwww", 
+  "展開だらだらしすぎ"
+];
+
 const correct = "信じることだ";
 
 document.getElementById("js-question").textContent = question;
 
-document.getElementsByTagName("button")[0].textContent = answers[0];
-document.getElementsByTagName("button")[1].textContent = answers[1];
-document.getElementsByTagName("button")[2].textContent = answers[2];
-document.getElementsByTagName("button")[3].textContent = answers[3];
+const button = document.getElementsByTagName("button");
+button[0].textContent = answers[0];
+button[1].textContent = answers[1];
+button[2].textContent = answers[2];
+button[3].textContent = answers[3];
 
 document.getElementsByTagName("button")[0].addEventListener("click", ()=>{
   if( correct === document.getElementsByTagName("button")[0].textContent){
