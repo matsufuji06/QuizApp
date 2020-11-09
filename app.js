6c5a28ab3aca0d14812a91ded5f56e4d67c7b15e
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-09 21:49:10 +0900

    e.targetでリファクタリング

--- app.js
+++ app.js
@@ -20,31 +20,33 @@ const setupQuiz = () => {
   };
 }
 
-setupQuiz()
+setupQuiz();
 
-$button[0].addEventListener("click", ()=>{
-  if( correct === $button[0].textContent){
+
+
+$button[0].addEventListener("click", (e)=>{
+  if( correct === e.target.textContent){
     window.alert("正解！");
   } else {
     window.alert("不正解！");
   }
 });
-$button[1].addEventListener("click", ()=>{
-  if( correct === $button[1].textContent){
+$button[1].addEventListener("click", (e)=>{
+  if( correct === e.target.textContent){
     window.alert("正解！");
   } else {
     window.alert("不正解！");
   }
 });
-$button[2].addEventListener("click", ()=>{
-  if( correct === $button[2].textContent){
+$button[2].addEventListener("click", (e)=>{
+  if( correct === e.target.textContent){
     window.alert("正解！");
   } else {
     window.alert("不正解！");
   }
 });
-$button[3].addEventListener("click", ()=>{
-  if( correct === $button[3].textContent){
+$button[3].addEventListener("click", (e)=>{
+  if( correct === e.target.textContent){
     window.alert("正解！");
   } else {
     window.alert("不正解！");
