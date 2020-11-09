bd06f18566c36e42f4847c6ce3f5bb2c82bda452
matsufuji06 <n.matsufuji06@gmail.com>
2020-11-09 21:46:13 +0900

    関数でクイズの問題文と答えの選択肢を定義

--- app.js
+++ app.js
@@ -7,22 +7,20 @@ const answers = [
 ];
 
 const correct = "信じることだ";
-
-document.getElementById("js-question").textContent = question;
-
 const $button = document.getElementsByTagName("button");
 
-// $button[0].textContent = answers[0];
-// $button[1].textContent = answers[1];
-// $button[2].textContent = answers[2];
-// $button[3].textContent = answers[3];
+// クイズの問題文と答えの選択肢を定義
+const setupQuiz = () => {
+  document.getElementById("js-question").textContent = question;
+  let buttonIndex = 0;
+  let buttonLength = $button.length;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  };
+}
 
-let buttonIndex = 0;
-let buttonLength = $button.length;
-while(buttonIndex < buttonLength){
-  $button[buttonIndex].textContent = answers[buttonIndex];
-  buttonIndex++;
-};
+setupQuiz()
 
 $button[0].addEventListener("click", ()=>{
   if( correct === $button[0].textContent){
